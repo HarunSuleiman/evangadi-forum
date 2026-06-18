cb913fdc9092dfbffbb6429bac1ecc3e98efdc20
HarunSuleiman <harunsuleiman55@gmail.com>
2026-06-18 22:16:44 +0300

    registration backend is completed

--- evangadi-forum/app.js
+++ evangadi-forum/app.js
@@ -6,6 +6,8 @@ const port = 3000;
 //db connection
 const dbConnection = require("./db/dbConfig");
 
+
+
 //tocheck on the connection to the database on the browser
 app.get("/", (req, res) => {
   res.send("Welcome to the Evangadi Forum API");
@@ -13,13 +15,18 @@ app.get("/", (req, res) => {
 
 // user routes middleware
 const userRoutes = require("./routes/userRout");
+//middleware to parse JSON request bodies it is before the user routes middleware so that the request body can be parsed before it reaches the user routes
+app.use(express.json());
+
 app.use("/api/users", userRoutes);
 
 async function start() {
   try {
     // Test the database connection
     const result = await dbConnection.execute("select 'test' ");
-    await app.listen(port);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
     console.log("Database connection successful");
     console.log(`Server is running on port ${port}`);
   } catch (error) {
--- evangadi-forum/controller/userController.js
+++ evangadi-forum/controller/userController.js
@@ -1,10 +1,51 @@
-function register(req, res) {
-  res.send("register");
+const dbConnection = require("../db/dbConfig");
+
+//importing bcrypt
+const bcrypt = require("bcrypt");
+// const { statusCodes } = require("http-status-codes");
+
+async function register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
+  if (!username || !firstname || !lastname || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Check if the user already exists
+    if (password.length < 8) {
+      return res.status(400).json({
+        message: "password must be at least 8 characters",
+      });
+    }
+    const [user] = await dbConnection.query(
+      "select username,userid from users where username=? or email=?",
+      [username, email],
+    );
+    console.log(user);
+    if (user.length > 0) {
+      return res.status(400).json({ message: "user alresdy registered " });
+    }
+
+    //encript the password
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(password, salt);
+
+    await dbConnection.query(
+      "INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)",
+      [username, firstname, lastname, email, hashpassword],
+    );
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Server error" });
+  }
 }
-function login(req, res) {
-  res.send("login");
+async function login(req, res) {
+  // login using email and password 
+  
+
 }
-function checkUser(req, res) {
+async function checkUser(req, res) {
   res.send("check");
 }
 module.exports = { register, login, checkUser };
